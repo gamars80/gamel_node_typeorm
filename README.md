884d6a1cc73544391e66787dc03b42c94cc16e46
gamars <gamars80@gmail.com>
2023-11-21 10:39:15 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Awesome Project Build with TypeORM
+
+Steps to run this project:
+
+1. Run `npm i` command
+2. Setup database settings inside `data-source.ts` file
+3. Run `npm start` command
+
+
+# TypeORM 사용
+    tsconfig.ts : 타입스크립트로 짜인 코드를 javascript로 콤파일 하는 옵션 설장 파일
+    npx tsc --init 하면 생성됨
+
+    tsconfig.json 에 esModuleInterop: true로 추가
+
+    morgan모듈 설치: log 관리
+
+    ts-node : Node.js 상에서 타입스크립트 콘트롤러를 통하지 않고 직접 타입스크립트를 실행하는 역할
+
+# 데이터 베이스
+    npm install pg typeorm reflect-metadata --save
+
+    reflect-metadata 모듈 : @어노테이션 사용응 위한
+
+    pg 모듈: postgres 데이터베이스 드라이버
+
+    npx typeorm init : data-source.ts 생성 (데이터베이스 접근을 위한 옵션 설정들)
+
+# Docker 설치
+    docker-compose.yml 생성
+
+    docker desktop 떠있는 상태로 프로젝트에서 docker-compose up 명령어 실행
+
+    pgAdmin 설치 : postgres db 매니저 툴
+
+    
+
---
+++ src/index.ts
@@ -0,0 +1,65 @@
+import express from 'express';
+import morgan from 'morgan';
+import { AppDataSource } from './data-source';
+import { User } from './entity/User';
+
+const app = express();
+
+//미들웨어
+app.use(express.json());
+//로그 미들웨어 장착 dev, short, common, combined
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.send('running');
+})
+
+AppDataSource
+    .initialize()
+    .then(() => {
+        console.log('성공')
+    })
+    .catch((err) => {
+        console.error(err);
+    })
+
+    app.post('/users', async (req, res) => {
+        const user = await AppDataSource.getRepository(User).create(req.body);
+        console.log(user);
+
+        const results = await AppDataSource.getRepository(User).save(user);
+        return res.send(results);
+    });
+
+    app.get('/users', async (req, res) => {
+        const results = await AppDataSource.getRepository(User).find();
+        res.json(results);
+    });
+
+    app.get('/users/:id', async (req, res) => {
+        const results = await AppDataSource.getRepository(User).findOneBy({
+            id: Number(req.params.id)
+        });
+
+        return res.json(results);
+    });
+
+    app.put('/users/:id',async (req, res) => {
+        const user = await AppDataSource.getRepository(User).findOneBy({
+            id: Number(req.params.id)
+        });      
+
+        AppDataSource.getRepository(User).merge(user, req.body);
+
+        const result = await AppDataSource.getRepository(User).save(user);
+        return res.send(result);
+    });
+
+    app.delete('/users/:id', async (req, res) =>{
+        const result = await AppDataSource.getRepository(User).delete(req.params.id);
+        res.json(result);
+    })
+const port = 4000;
+app.listen(port, () => {
+    console.log(`Server Running at http://localhost:${port}`);
+})
